adb159b67eb5346f9bdab5cb68aced3ebfbb6d67
Max Jones <14077947+maxrjones@users.noreply.github.com>
2024-08-01 15:25:18 -0400

    Update VS Code extension

--- install-vscode-ext.sh
+++ install-vscode-ext.sh
@@ -3,8 +3,9 @@
 # Install VSCode extensions.
 # These get installed to $CONDA_PREFIX/envs/notebook/share/code-server/extensions/
 
-extensions=("ms-python.python" "ms-toolsai.jupyter" "ms-toolsai.vscode-jupyter-powertoys" "ms-python.debugpy" "eamodio.gitlens" "wholroyd.jinja" "esbenp.prettier-vscode" "njpwerner.autodocstring" "quarto.quarto")
+extensions=("ms-toolsai.jupyter" "ms-toolsai.vscode-jupyter-powertoys" "ms-python.debugpy" "eamodio.gitlens" "esbenp.prettier-vscode" "njpwerner.autodocstring" "quarto.quarto" "ms-python.python")
 
 for EXT in "${extensions[@]}"; do
+    code-server --version
     code-server --install-extension "${EXT}"
 done
